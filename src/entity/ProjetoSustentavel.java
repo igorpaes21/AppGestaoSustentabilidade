46adbcecd35308d2f1619bf0f51774a51b8b74c4
igorpaes21 <irsp2121@hmail.com>
2025-03-23 23:46:40 -0300

    projeto finalizado

--- src/entity/ProjetoSustentavel.java
+++ src/entity/ProjetoSustentavel.java
@@ -1,29 +1,28 @@
 package entity;
 
+import interfaces.ImpactoAmbiental;
+
 import java.util.ArrayList;
 
-public class ProjetoSustentavel {
+public abstract class ProjetoSustentavel implements ImpactoAmbiental {
+    String nome;
+    String descricao;
+    private RelatorioImpacto relatorioImpacto;
+    private static final ArrayList<Voluntario> voluntarios = new ArrayList<>();
 
-    private String nome;
-    private String descricao;
-    private String relatorioImpacto;
-    ArrayList<Voluntario> voluntarios = new ArrayList<>();
+    public ProjetoSustentavel() {
+    }
 
-    ProjetoSustentavel(){}
-
-    public ProjetoSustentavel(String descricao, String nome, String relatorioImpacto, ArrayList<Voluntario> voluntarios) {
-        this.descricao = descricao;
+    public ProjetoSustentavel(String nome, String descricao) {
         this.nome = nome;
-        this.relatorioImpacto = relatorioImpacto;
-        this.voluntarios = voluntarios;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
         this.descricao = descricao;
+        this.relatorioImpacto = new RelatorioImpacto();
+    }
+
+    public ProjetoSustentavel(String nome, String descricao, ArrayList<Voluntario> voluntarios) {
+        this.nome = nome;
+        this.descricao = descricao;
+
     }
 
     public String getNome() {
@@ -34,11 +33,20 @@ public class ProjetoSustentavel {
         this.nome = nome;
     }
 
-    public String getRelatorioImpacto() {
-        return relatorioImpacto;
+    public String getDescricao() {
+        return descricao;
     }
 
-    public void setRelatorioImpacto(String relatorioImpacto) {
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public RelatorioImpacto getRelatorioImpacto() {
+        return relatorioImpacto;
+
+    }
+
+    public void setRelatorioImpacto(RelatorioImpacto relatorioImpacto) {
         this.relatorioImpacto = relatorioImpacto;
     }
 
@@ -46,13 +54,49 @@ public class ProjetoSustentavel {
         return voluntarios;
     }
 
-    public void setVoluntarios(ArrayList<Voluntario> voluntarios) {
-        this.voluntarios = voluntarios;
-    }
-    public static void adicionarVoluntario(Voluntario voluntario){
+    public int getArvoresPlantadas() {
+        return relatorioImpacto.getArvoresPlantadas();
 
     }
-    public static void gerarRelatorioImpacto(int arvoresPlantadas, double reducaoCO2){
 
+    public double getReducaoCo2() {
+        return relatorioImpacto.getReducaoCo2();
+    }
+
+    public void addVoluntario(Voluntario voluntario) {
+        voluntarios.add(voluntario);
+    }
+
+    public void gerarRelatorioImpacto(int arvoresPlantadas, double reducaoCo2) {
+        this.relatorioImpacto = new RelatorioImpacto(arvoresPlantadas, reducaoCo2);
+    }
+
+    public void calcularImpacto(int arvoresPlantadas, double reducaoCo2) {
+        gerarRelatorioImpacto(arvoresPlantadas, reducaoCo2);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder voluntariosNomes = new StringBuilder();
+        for (Voluntario voluntario : voluntarios) {
+            voluntariosNomes.append(voluntario.getNome()).append(", ");
+        }
+        if (!voluntariosNomes.isEmpty()) {
+            voluntariosNomes.setLength(voluntariosNomes.length() - 2);
+        }
+
+        return "ProjetoSustentavel{" +
+                "nome = '" + nome + '\'' +
+                ", descricao = '" + descricao + '\'' +
+                ", relatorio Impacto = " + relatorioImpacto +
+                ", voluntarios = " + voluntariosNomes +
+                '}';
+    }
+
+
+    public void ListarVoluntarios() {
+        for (Voluntario voluntario : voluntarios) {
+            System.out.println(voluntario);
+        }
     }
 }
